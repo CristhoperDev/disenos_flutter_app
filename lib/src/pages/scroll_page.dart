2f981be643502c2edcc746cb778bfd6fc68d3664
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-30 18:25:10 -0500

    scroll design part 02

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -15,8 +15,50 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(
-        child: Text('Pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _backgroundColor(),
+        _backgroundImage(),
+        _texts(),
+      ],
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color.fromRGBO(108, 192, 219, 1.0));
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _texts() {
+    final _textStyle = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(children: <Widget>[
+        SizedBox(
+          height: 20.0,
+        ),
+        Text('11°', style: _textStyle),
+        Text('Miércoles', style: _textStyle),
+        Expanded(child: Container()),
+        Icon(
+          Icons.keyboard_arrow_down,
+          size: 70.0,
+          color: Colors.white,
+        )
+      ]),
     );
   }
 
