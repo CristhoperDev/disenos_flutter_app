2e612b9add9c311d02a6d8b52428853ea77e92e5
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-30 17:58:59 -0500

    basic design part 03

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -7,45 +7,54 @@ class BasicPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          _createImage(),
-          _createTitle(),
-          _createActions(),
-          _createText(),
-          _createText(),
-          _createText(),
-          _createText(),
-          _createText(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _createImage(),
+            _createTitle(),
+            _createActions(),
+            _createText(),
+            _createText(),
+            _createText(),
+            _createText(),
+            _createText(),
+          ],
+        ),
       ),
     );
   }
 
   Widget _createImage() {
-    return Image(
-      image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        fit: BoxFit.cover,
+        height: 200.0,
+        image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+      ),
     );
   }
 
   Widget _createTitle() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Lago con un puente', style: _titleStyle),
-                SizedBox(height: 5.0),
-                Text('Un lago en alemania', style: _subTitleStyle),
-              ],
+      child: SafeArea(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: _titleStyle),
+                  SizedBox(height: 5.0),
+                  Text('Un lago en alemania', style: _subTitleStyle),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0,),
-          Text('41', style: TextStyle(fontSize: 20.0),)
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0,),
+            Text('41', style: TextStyle(fontSize: 20.0),)
+          ],
+        ),
       ),
     );
   }
@@ -72,11 +81,13 @@ class BasicPage extends StatelessWidget {
   }
 
   Widget _createText() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Text(
-        'Sed fermentum sem nec commodo sagittis. Donec tincidunt elit libero, eget posuere elit sodales ac. In hac habitasse platea dictumst. Integer aliquam justo lorem, sit amet lobortis mi pellentesque vel. Mauris nec sapien quis tellus finibus mattis. Ut ullamcorper turpis vel feugiat iaculis. Cras vestibulum nulla at enim vestibulum sollicitudin. Quisque posuere mattis nisi, sed venenatis arcu cursus et. Donec eget consequat tellus.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        child: Text(
+          'Sed fermentum sem nec commodo sagittis. Donec tincidunt elit libero, eget posuere elit sodales ac. In hac habitasse platea dictumst. Integer aliquam justo lorem, sit amet lobortis mi pellentesque vel. Mauris nec sapien quis tellus finibus mattis. Ut ullamcorper turpis vel feugiat iaculis. Cras vestibulum nulla at enim vestibulum sollicitudin. Quisque posuere mattis nisi, sed venenatis arcu cursus et. Donec eget consequat tellus.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
