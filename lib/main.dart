38a4e3739b6d80e709f92b499bc01e3a74cb8250
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-30 19:19:37 -0500

    complex design background, gradients, statusbar, titles

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:disenos_flutter_app/src/pages/basic_page.dart';
+import 'package:disenos_flutter_app/src/pages/buttons_page.dart';
 import 'package:disenos_flutter_app/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.white
+    ) );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: '/scroll',
+      initialRoute: '/buttons',
       routes: {
-        '/basico' : (BuildContext context) => BasicPage(),
-        '/scroll' : (BuildContext context) => ScrollPage(),
+        '/basico'   : (BuildContext context) => BasicPage(),
+        '/scroll'   : (BuildContext context) => ScrollPage(),
+        '/buttons'  : (BuildContext context) => ButtonsPage(),
       },
     );
   }
