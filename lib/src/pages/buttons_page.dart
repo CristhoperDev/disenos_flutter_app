998028f02e40dcb7de8a1fbffce269f9bb507a25
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-30 19:32:51 -0500

    complex design bottom navigation bar

--- lib/src/pages/buttons_page.dart
+++ lib/src/pages/buttons_page.dart
@@ -21,6 +21,7 @@ class ButtonsPage extends StatelessWidget {
           ],
         )
       ),
+      bottomNavigationBar: _bottonNavigationBar(context),
     );
   }
 
@@ -83,6 +84,32 @@ class ButtonsPage extends StatelessWidget {
             Text('Classify this transaction into a particular category', style: TextStyle(color:  Colors.white, fontSize: 18.0)),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _bottonNavigationBar(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme.copyWith(caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0))),
+      ),
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bubble_chart, size: 30.0),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle, size: 30.0),
+            title: Container(),
+          ),
+        ],
       ),
     );
   }
